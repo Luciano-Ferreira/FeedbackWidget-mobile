35bb2a0b55ce9b9562eefb204be381e5ea179a8e
Luciano Ferreira <luciano2023silva@gmail.com>
2022-05-08 01:33:46 -0300

    stage 4 :rocket:

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts, Inter_500Medium, Inter_400Regular } from '@expo-google-fonts/inter';
+import Widget from './src/components/Widget';
+import { theme } from './src/theme';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+
+      <StatusBar
+        style='light'
+        backgroundColor='transparent'
+        translucent
+      />
+
+      <Widget />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
